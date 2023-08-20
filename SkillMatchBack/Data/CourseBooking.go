69b79f9c9a8d8d622f9bb5269080c548f3f8006e
vekt0R-HUB <zealot.algo@gmail.com>
2023-08-20 12:47:16 -0400

    Removed unnecessary files

--- SkillMatchBack/Data/CourseBooking.go
+++
@@ -1,9 +0,0 @@
-package Data
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type CourseBooking struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	CourseId primitive.ObjectID `bson:"courseId,omitempty"`
-	UserId   primitive.ObjectID `bson:"userId,omitempty"`
-}
--- SkillMatchBack/Data/User.go
+++
@@ -1,9 +0,0 @@
-package Data
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `json:"username"`
-	Password string             `json:"password"`
-}
